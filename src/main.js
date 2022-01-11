c60068f420e12adb05a98aee6e5d1883cce490de
mmarienko <maksym.marienko@gmail.com>
2022-01-11 04:05:04 +0200

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+import { initializeApp } from 'firebase/app';
 import App from './App.vue'
 import store from './store'
 import router from './router'
+
+initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+});
 
 createApp(App).use(router).use(store).mount('#app')
